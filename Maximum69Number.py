59d9a642d3408638235e2144333681de4d0e9c95
nishantyaji <nishant.yaji@gmail.com>
2022-06-25 14:33:26 +0530

    solution to leetcode problem 1323

---
+++ Maximum69Number.py
@@ -0,0 +1,25 @@
+# Problem 1323
+
+
+from cmath import log10
+
+from numpy import result_type
+
+
+class Maximum69Number:
+    def maximum69Number(self, num: int) -> int:
+        resultStr = ""
+        found = False
+        for character in str(num):
+            if found == False and character == "6":
+                first = "9"
+                found = True
+            else:
+                first = character
+            resultStr = resultStr + first
+        return int(resultStr)
+
+
+if __name__ == '__main__':
+    m = Maximum69Number()
+    print(m.maximum69Number(9669))
