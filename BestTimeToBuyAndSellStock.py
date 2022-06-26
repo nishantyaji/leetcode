3f719c3b60d8c170103d8354e611f050aa0e570a
nishantyaji <nishant.yaji@gmail.com>
2022-06-26 20:31:32 +0530

    Solution to Leetcode problem 121

---
+++ BestTimeToBuyAndSellStock.py
@@ -0,0 +1,21 @@
+# Problem 121
+from typing import List
+
+class BestTimeToBuyAndSellStock:    
+    def maxProfit(self, prices: List[int]) -> int:
+        min = 1e6
+        max_profit = 0
+        for num in prices:
+            if num < min:
+                min = num
+            else:
+                temp = num - min
+                if temp > max_profit:
+                    max_profit = temp
+        return max_profit
+                
+
+if __name__ == '__main__':
+    b = BestTimeToBuyAndSellStock()
+    print(b.maxProfit([7,1,5,3,6,4]))
+    print(b.maxProfit([7,6,4,3,1]))
