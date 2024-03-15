4fdb2f88b4edd904029d73538b38c53310c0ac8b
nishantyaji <nishant.yaji@gmail.com>
2024-03-15 09:11:58 +0530

    Solution to leet code problem 238

---
+++ ProductOfArrayExceptSelf.py
@@ -0,0 +1,27 @@
+#Problem 238
+
+class ProductOfArrayExceptSelf:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        nonzeroproduct = 1
+        numzeroes = 0
+
+        for i in nums:
+            if i == 0:
+                numzeroes += 1
+            else:
+                nonzeroproduct *= i
+
+        if numzeroes > 1:
+            return [0] * len(nums)
+        else:
+            res = []
+            for i in nums:
+                if i == 0:
+                    res.append(nonzeroproduct)
+                else:
+                    if numzeroes > 0:
+                        res.append(0)
+                    else:
+                        res.append((int)(nonzeroproduct/i))
+        return res
