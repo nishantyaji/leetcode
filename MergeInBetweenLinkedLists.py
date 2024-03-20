0e761d5734eb059c740672ed33dfc2e5d9a59420
nishantyaji <nishant.yaji@gmail.com>
2024-03-20 17:36:46 +0530

    Solution to leet code Problem 1669

---
+++ MergeInBetweenLinkedLists.py
@@ -0,0 +1,29 @@
+#Problem 1669
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MergeInBetweenLinkedLists:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        
+        list2present = list2
+        while list2present.next != None:
+            list2present = list2present.next
+        list2end = list2present
+        
+        
+        list1present = list1
+        for i in range(0, b):
+            print("-" + list1present.val)
+            list1next = list1present.next
+            if i == a:
+                list1present.next = list2
+            
+        list2end.next = list1next
+        
+        return list1
+    
+if __name__ == '__main__':
+    m = MergeInBetweenLinkedLists()
