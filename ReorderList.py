12769eff42ffec4e9b3749a78a12b81bfa3e5124
nishantyaji <nishant.yaji@gmail.com>
2024-03-23 23:50:42 +0530

    Solution to the leet code problem 143

---
+++ ReorderList.py
@@ -0,0 +1,42 @@
+# Problem 143
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class ReorderList:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if head is None or head.next is None or head.next.next is None:
+            return
+        
+        mylist = []
+        present = head
+        while present is not None:
+            mylist.append(present)
+            present = present.next
+
+        for node in mylist:
+            node.next = None
+            
+        halflimit = (int) (len(mylist) / 2)
+
+        forward = 0
+        backward = 0
+
+        while forward < len(mylist) + backward - 1:
+            if backward < 0:
+                mylist[backward].next = mylist[forward]
+            backward -=1
+            mylist[forward].next = mylist[backward]
+            forward += 1
+        
+        if len(mylist) % 2 == 1:
+            mylist[backward].next = mylist[forward]
+        
+        
+if __name__ == '__main__':
+    r = ReorderList()
