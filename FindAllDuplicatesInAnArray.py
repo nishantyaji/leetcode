632fc9c85d8ae71c02cf00154d4badb40f99b1ce
nishantyaji <nishant.yaji@gmail.com>
2024-03-25 11:38:55 +0530

    Solution to leet code problem 442

---
+++ FindAllDuplicatesInAnArray.py
@@ -0,0 +1,21 @@
+#Problem 442
+
+class FindAllDuplicatesInAnArray:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        mylist = []
+        for i in range(1, len(nums) + 1):
+            mylist.append(i)
+        myset = set(mylist)
+
+        result = []
+        for i in nums:
+            if i in myset:
+                myset.remove(i)
+            else:
+                result.append(i)
+
+        return result
+
+if __name__ == '__main__':
+    f = FindAllDuplicatesInAnArray()
+    print(f.findDuplicates([4,3,2,7,8,2,3,1]))
