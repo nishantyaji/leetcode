c2e5afe92bc727dcb58508ca10a511bfbade936e
nishantyaji <nishant.yaji@gmail.com>
2024-04-26 10:57:34 +0530

    Solution to the leet code problem 1289

---
+++ MinimumFallingPathSumII.py
@@ -0,0 +1,30 @@
+# Problem 1289
+
+from typing import List
+
+
+class MinimumFallingPathSumII:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        cumsum = a = [[0 for _ in range(0, rows)] for y in range(0, cols)]
+
+        for i in range(0, cols):
+            cumsum[0][i] = grid[0][i]
+
+        for i in range(1, rows):
+            for j in range(0, cols):
+                min_list = []
+                if j > 0:
+                    min_list.append(min(cumsum[i - 1][:j]))
+                if j < cols - 1:
+                    min_list.append(min(cumsum[i - 1][j + 1:]))
+
+                cumsum[i][j] = grid[i][j] + min(min_list)
+
+        return min(cumsum[rows - 1])
+
+
+if __name__ == '__main__':
+    m = MinimumFallingPathSumII()
+    print(m.minFallingPathSum([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    # print(m.minFallingPathSum())
