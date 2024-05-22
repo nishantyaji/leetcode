f0867f310a5ffd2ea943ea4a52a94fa0639690fd
nishantyaji <nishant.yaji@gmail.com>
2024-05-22 21:23:33 +0530

    Solution to the leet code problem 3151

---
+++ SpecialArrayI.py
@@ -0,0 +1,16 @@
+# 3151. Special Array I
+
+from typing import List
+
+
+class SpecialArrayI:
+
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(len(nums) - 1):
+            if (nums[i] - nums[i + 1]) % 2 == 0:
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    w = SpecialArrayI()
