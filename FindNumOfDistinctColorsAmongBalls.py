48bdbe29ebdbf85107da215b021e146101cdf5c8
nishantyaji <nishant.yaji@gmail.com>
2024-06-09 12:21:20 +0530

    Solution to the leet code problem 3160

---
+++ FindNumOfDistinctColorsAmongBalls.py
@@ -0,0 +1,27 @@
+# Problem 3160 : Find the number of Distinct colors among the balls
+
+import collections
+from typing import List
+
+
+class FindNumOfDistinctColorsAmongBalls:
+    def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+        my_map = dict()
+        dict_of_set = collections.defaultdict(set)
+        result = []
+        for q in queries:
+            if q[0] in my_map:
+                q1prev = my_map[q[0]]
+                dict_of_set[q1prev].remove(q[0])
+                if len(dict_of_set[q1prev]) == 0:
+                    del dict_of_set[q1prev]
+            my_map[q[0]] = q[1]
+            dict_of_set[q[1]].add(q[0])
+            result.append(len(dict_of_set))
+        return result
+
+
+if __name__ == '__main__':
+    b = BiweeklyContest131P3()
+    print(b.queryResults(4, [[1, 4], [2, 5], [1, 3], [3, 4]]))
+    print(b.queryResults(4, [[0, 1], [1, 2], [2, 2], [3, 4], [4, 5]]))
