adc7aa27fbb1717185563d73f50e28b643e914eb
nishantyaji <nishant.yaji@gmail.com>
2024-06-16 19:45:48 +0530

    Solution to the leet code 101 (fixed import and refactored 2 boolean expressions)

--- SymmetricTree.py
+++ SymmetricTree.py
@@ -1,45 +1,50 @@
 # Problem 101
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-        
+
+
 class SymmetricTree:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if root is None:
             return True
-        
+
         if (root.left is None and root.right is not None) or (root.left is not None and root.right is None):
             return False
         elif root.left is None and root.right is None:
             return True
         else:
             return self.areEqual(root.left, root.right)
-        
+
     def areEqual(self, leftside: TreeNode, rightside: TreeNode) -> bool:
         if leftside.val != rightside.val:
             return False
-        
+
         if leftside.left is None and rightside.right is None:
-            #do nothing
-            pass 
+            # do nothing
+            pass
         elif leftside.left is not None and rightside.right is not None:
-            if self.areEqual(leftside.left, rightside.right) == False:
+            if not self.areEqual(leftside.left, rightside.right):
                 return False
         else:
             return False
-        
+
         if leftside.right is None and rightside.left is None:
-            #do nothing
+            # do nothing
             pass
         elif leftside.right is not None and rightside.left is not None:
-            if self.areEqual(leftside.right, rightside.left) == False:
+            if not self.areEqual(leftside.right, rightside.left):
                 return False
         else:
             return False
-        
+
         return True
-    
+
+
 if __name__ == '__main__':
     s = SymmetricTree()
