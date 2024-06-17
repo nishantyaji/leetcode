3f7dadb7976e4251da8d93c36b1147c1efe79415
nishantyaji <nishant.yaji@gmail.com>
2024-06-17 10:26:57 +0530

    Renaming files to have problem numbers as prefix (a decision I should have taken long ago)

--- 0121_BestTimeToBuyAndSellStock.py
+++ 0121_BestTimeToBuyAndSellStock.py
@@ -1,7 +1,8 @@
 # Problem 121
 from typing import List
 
-class BestTimeToBuyAndSellStock:    
+
+class BestTimeToBuyAndSellStock:
     def maxProfit(self, prices: List[int]) -> int:
         min = 1e6
         max_profit = 0
@@ -13,9 +14,9 @@ class BestTimeToBuyAndSellStock:
                 if temp > max_profit:
                     max_profit = temp
         return max_profit
-                
+
 
 if __name__ == '__main__':
     b = BestTimeToBuyAndSellStock()
-    print(b.maxProfit([7,1,5,3,6,4]))
-    print(b.maxProfit([7,6,4,3,1]))
+    print(b.maxProfit([7, 1, 5, 3, 6, 4]))
+    print(b.maxProfit([7, 6, 4, 3, 1]))
