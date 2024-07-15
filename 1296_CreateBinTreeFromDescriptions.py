670477de5cbc9ade5f9b84f77ba05015b1504478
nishantyaji <nishant.yaji@gmail.com>
2024-07-15 11:37:48 +0530

    Solution to the leet code problem 1296

---
+++ 1296_CreateBinTreeFromDescriptions.py
@@ -0,0 +1,35 @@
+# Problem 2196
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class CreateBinTreeFromDescriptions:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        mp, parent_set, children_set = dict(), set(), set()
+        for d in descriptions:
+            [parent, child, is_left] = d
+            parent_set.add(parent)
+            children_set.add(child)
+            if parent not in mp:
+                parent_node = TreeNode(parent)
+                mp[parent] = parent_node
+            if child not in mp:
+                child_node = TreeNode(child)
+                mp[child] = child_node
+            if is_left == 1:
+                mp[parent].left = mp[child]
+            else:
+                mp[parent].right = mp[child]
+        return mp[list(parent_set - children_set)[0]]
+
+
+if __name__ == '__main__':
+    c = CreateBinTreeFromDescriptions()
+    print(c.createBinaryTree([[20, 15, 1], [20, 17, 0], [50, 20, 1], [50, 80, 0], [80, 19, 1]]))
+    print(c.createBinaryTree([[1, 2, 1], [2, 3, 0], [3, 4, 1]]))
