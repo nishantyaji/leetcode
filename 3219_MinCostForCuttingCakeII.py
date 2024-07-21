70fe48aa0c1170904f5794d9c12f29a74d786aa6
nishantyaji <nishant.yaji@gmail.com>
2024-07-22 00:29:12 +0530

    Solution to the leet code problem 3219 (Weekly contest 406, Q4 hard)(I did not participate in the contest)

---
+++ 3219_MinCostForCuttingCakeII.py
@@ -0,0 +1,29 @@
+# Problem 3219
+import operator
+from typing import List
+
+
+class MinCostForCuttingCakeII:
+
+    def minimumCost(self, m: int, n: int, horizontalCut: List[int], verticalCut: List[int]) -> int:
+
+        h_tuples = [[x, "h"] for x in horizontalCut]
+        v_tuples = [[x, "v"] for x in verticalCut]
+        tuples = h_tuples + v_tuples
+        tuples.sort(key=operator.itemgetter(0), reverse=True)
+        h_count, v_count, cost = 1, 1, 0
+        for t in tuples:
+            if t[1] == "h":
+                mul = v_count
+                h_count += 1
+            else:
+                mul = h_count
+                v_count += 1
+            cost += t[0] * mul
+        return cost
+
+
+if __name__ == '__main__':
+    m = MinCostForCuttingCakeII()
+    print(m.minimumCost(3, 2, [1, 3], [5]))
+    print(m.minimumCost(2, 2, [7], [4]))
