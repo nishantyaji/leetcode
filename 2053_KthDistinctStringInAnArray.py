186ea3df58b5bc01ee21e9630804f0eef660ddbf
nishantyaji <nishant.yaji@gmail.com>
2024-08-05 09:21:19 +0530

    Solution to the leet code problem 2053

---
+++ 2053_KthDistinctStringInAnArray.py
@@ -0,0 +1,18 @@
+# 2053_KthDistinctStringInAnArray
+from typing import List
+
+
+class KthDistinctStringInAnArray:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        ones, more = [], set()
+
+        for i in arr:
+            if i in more:
+                continue
+            elif i in ones:
+                ones.remove(i)
+                more.add(i)
+            else:
+                ones.append(i)
+
+        return "" if k > len(ones) else ones[k - 1]
