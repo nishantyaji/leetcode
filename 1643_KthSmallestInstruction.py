561949313982bd766e36e39813674727b090747f
nishantyaji <nishant.yaji@gmail.com>
2024-10-13 23:00:51 +0530

    Solution to the leet code problem 1643

---
+++ 1643_KthSmallestInstruction.py
@@ -0,0 +1,32 @@
+# Problem 1643
+import functools
+import math
+from typing import List
+
+
+class KthSmallestInstruction:
+    def kthSmallestPath(self, destination: List[int], k: int) -> str:
+        running = self.recurse(destination[0], destination[1], k)
+        return "".join(running)
+
+    @functools.cache
+    def recurse(self, r: int, c: int, k: int):
+        if c == 0:
+            return ["V"] * r
+        elif r == 0:
+            return ["H"] * c
+        limit = math.comb(r + c - 1, c - 1)
+        # after the limit, the leading letter will be "V"
+        if k > limit:
+            k -= limit
+            return ["V"] + self.recurse(r - 1, c, k)
+        else:
+            return ["H"] + self.recurse(r, c - 1, k)
+
+
+if __name__ == '__main__':
+    k = KthSmallestInstruction()
+    print(k.kthSmallestPath([15, 2], 136))  # "VVVVVVVVVVVVVVVHH"
+    print(k.kthSmallestPath([2, 3], 1))
+    print(k.kthSmallestPath([2, 3], 2))
+    print(k.kthSmallestPath([2, 3], 3))
