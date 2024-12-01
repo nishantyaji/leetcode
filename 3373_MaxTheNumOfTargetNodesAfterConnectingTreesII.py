e98ad2ce86cfe9581fbbca22daca9289b8defde5
nishantyaji <nishant.yaji@gmail.com>
2024-12-01 11:42:33 +0530

    Solution to Leet code problem 3373 (in a more concise code)

--- 3373_MaxTheNumOfTargetNodesAfterConnectingTreesII.py
+++ 3373_MaxTheNumOfTargetNodesAfterConnectingTreesII.py
@@ -14,38 +14,26 @@ class MaxTheNumOfTargetNodesAfterConnectingTreesII:
             adj1[e].append(s)
             tree1_sz = max(s, e, tree1_sz)
 
-        tree2_sz = 0
         for s, e in edges2:
             adj2[s].append(e)
             adj2[e].append(s)
-            tree2_sz = max(s, e, tree2_sz)
 
         def find_vals(adj):
             visited = set()
-            q = [(0, True)]  # (node, isZero)
-            zero, nonzero = set(), set()
+            q = [(0, 0)]  # (node, isZero)
+            set_list = [set(), set()]  # set_list[0] has nodes at even distance from 0,
             while q:
                 v, flag = q.pop()
+                set_list[flag % 2].add(v)
                 visited.add(v)
-                if flag:
-                    zero.add(v)
-                else:
-                    nonzero.add(v)
                 next_v = [(a, not flag) for a in adj[v] if a not in visited]
                 q = next_v + q
-            return [zero, nonzero]
+            return set_list
 
         [tree1_zero, tree1_nonzero] = find_vals(adj1)
         [tree2_zero, tree2_nonzero] = find_vals(adj2)
         max2 = max(len(tree2_zero), len(tree2_nonzero))
-
-        res = []
-        for i in range(tree1_sz + 1):
-            if i in tree1_zero:
-                res.append(max2 + len(tree1_zero))
-            else:
-                res.append(max2 + len(tree1_nonzero))
-        return res
+        return [max2 + (len(tree1_zero) if i in tree1_zero else len(tree1_nonzero)) for i in range(tree1_sz + 1)]
 
 
 if __name__ == '__main__':
