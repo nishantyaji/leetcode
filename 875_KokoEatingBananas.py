dd63adba08a3415dbb400f32116be65f839eacc6
nishantyaji <nishant.yaji@gmail.com>
2024-12-07 14:53:43 +0530

    Solution to leetcode problem 875

---
+++ 875_KokoEatingBananas.py
@@ -0,0 +1,38 @@
+# Problem 875
+import bisect
+import math
+from typing import List
+
+
+class KokoEatingBananas:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
+
+        s, e = 1, max(piles)
+        res = -1
+        while s <= e:
+            mid = (s + e) // 2
+            v = self.num_hours(piles, mid)
+            if v <= h:
+                res = mid
+                e = mid - 1
+            else:
+                s = mid + 1
+        return res
+
+    def num_hours(self, piles, speed: int):
+        idx = bisect.bisect_left(piles, speed)
+        if idx == len(piles):
+            return len(piles)
+
+        res = idx  # count the ones that are <= speed as 1 hour
+        for i in range(idx, len(piles)):
+            res += (math.ceil(piles[i] / speed))
+
+        return res
+
+if __name__ == '__main__':
+    k = KokoEatingBananas()
+    print(k.minEatingSpeed([3,6,7,11], 8))
+    print(k.minEatingSpeed([30,11,23,4,20], 5))
+    print(k.minEatingSpeed([30,11,23,4,20], 6))
