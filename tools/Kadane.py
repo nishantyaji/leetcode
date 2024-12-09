7995c0ac4835be161d9b8d642116cff8a67b0dc4
nishantyaji <nishant.yaji@gmail.com>
2024-12-09 22:37:47 +0530

    Add another alternative to Kadane algo

--- tools/Kadane.py
+++ tools/Kadane.py
@@ -9,5 +9,21 @@ def kadane_impl(x):
     return max_sum
 
 
+def presentprefix_minus_minprefix(x):
+
+    """
+    The answer is the max among
+    the prefix sum at index i and the min prefix sum that occurs before index i
+    """
+
+    min_prefix = 0
+    prefix = 0
+    max_sum = -sys.maxsize
+    for a in x:
+        prefix += a
+        min_prefix = min(min_prefix, prefix)
+        max_sum = max(max_sum, prefix - min_prefix)
+    return max_sum
+
 arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 print(kadane_impl(arr))
