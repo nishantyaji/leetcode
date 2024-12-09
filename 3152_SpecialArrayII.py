f2b8649d2511a72903b306dc810431b0c922e437
nishantyaji <nishant.yaji@gmail.com>
2024-12-09 08:47:23 +0530

    Newer solution to the leetcode problem 3152

--- 3152_SpecialArrayII.py
+++ 3152_SpecialArrayII.py
@@ -1,10 +1,12 @@
 # 3152. Special Array II
-
+import itertools
+import operator
 from typing import List
 
 
 class SpecialArrayII:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        # May 2024
         num_invalid = [0] * len(nums)
         for idx in range(1, len(nums)):
             if (nums[idx] + nums[idx - 1]) % 2 == 0:
@@ -20,6 +22,12 @@ class SpecialArrayII:
                 result.append(True)
 
         return result
+
+    def isArraySpecial2(self, nums: List[int], queries: List[List[int]]) -> List[bool]:\
+        # Dec 2024
+        flags = [int(not (nums[i] ^ nums[i + 1]) & 1) for i in range(len(nums) - 1)]
+        prefix = [0] + list(itertools.accumulate(flags, operator.add))
+        return [True if prefix[e] - prefix[s] == 0 else False for [s, e] in queries]
 
 
 if __name__ == '__main__':
