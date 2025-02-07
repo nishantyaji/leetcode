dff4ca497ac972e6262cfdc4204720923f083bab
nishantyaji <nishant.yaji@gmail.com>
2025-02-07 10:33:13 +0530

    Solution to the leetcode problem 3160

--- 3160_FindNumOfDistinctColorsAmongBalls.py
+++ 3160_FindNumOfDistinctColorsAmongBalls.py
@@ -5,7 +5,28 @@ from typing import List
 
 
 class FindNumOfDistinctColorsAmongBalls:
+
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+        mp, vals_dict = {}, {}
+
+        def change(index, val):  # Use count instead of set
+            if index in vals_dict:
+                prev_val = vals_dict[index]
+                mp[prev_val] = mp[prev_val] - 1
+                if mp[prev_val] == 0:
+                    del mp[prev_val]
+            if val not in mp:
+                mp[val] = 0
+            mp[val] += 1
+            vals_dict[index] = val
+            return len(mp)
+
+        res = []
+        for ball, color in queries:
+            res.append(change(ball, color))
+        return res
+
+    def queryResults_slow_old(self, limit: int, queries: List[List[int]]) -> List[int]:
         my_map = dict()
         dict_of_set = collections.defaultdict(set)
         result = []
