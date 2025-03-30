2cbd63afc38cdc40ca8030a5d9a97f7e7f71a0d0
nishantyaji <nishant.yaji@gmail.com>
2025-03-30 09:40:23 +0530

    Simple implementation of Binary Exponentiation (relatively faster way of calculating power)

--- tools/BinaryExponentiation_FastPower.py
+++ tools/BinaryExponentiation_FastPower.py
@@ -1,12 +1,15 @@
 # base = 1000000007
 
-def fast_pow(val: int, t: int, base: int):
+def fast_pow(val: int, t: int, mod_base: int):
+    if t > mod_base:
+        return fast_pow(val, t % mod_base, base)
+
     res = 1
     mult = val
     while t > 0:
         if t & 1 == 1:
-            res = (mult * res) % base
-        mult = (mult * mult) % base
+            res = (mult * res) % mod_base
+        mult = (mult * mult) % mod_base
         t = t >> 1
     return res
 
