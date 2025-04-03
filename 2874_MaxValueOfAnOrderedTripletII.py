a45533ef7c8c12b07f87050309b098da6398ce6d
nishantyaji <nishant.yaji@gmail.com>
2025-04-03 16:57:08 +0530

    Solution to leetcode problem 2874

---
+++ 2874_MaxValueOfAnOrderedTripletII.py
@@ -0,0 +1,18 @@
+# Problem 2873
+from typing import List
+
+
+class MaxValueOfAnOrderedTripletII:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+
+        arr = [0] * len(nums)
+        res = 0
+        mx = nums[-1]
+        for i in range(len(nums) - 1, -1, -1):
+            arr[i] = mx = max(mx, nums[i])
+        print(arr)
+        mx = nums[0]
+        for i in range(1, len(nums) - 1):
+            mx = max(mx, nums[i])
+            res = max((mx - nums[i]) * arr[i + 1], res)
+        return res
