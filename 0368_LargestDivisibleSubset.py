dc0e880cb7ddfea7451f3cbc254ac0482de9f7b7
nishantyaji <nishant.yaji@gmail.com>
2025-04-06 10:17:01 +0530

    Solution to the leetcode problem 368

--- 0368_LargestDivisibleSubset.py
+++ 0368_LargestDivisibleSubset.py
@@ -21,6 +21,21 @@ class LargestDivisibleSubset:
                             res = dp[j]
         return list(res)
 
+    def largestDivisibleSubset2(self, nums: List[int]) -> List[int]:
+        # use array instead of set
+        nums.sort()
+        dp = {i: [x] for i, x in enumerate(nums)}
+        max_len, res = 1, [nums[0]]
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[j] % nums[i] == 0:
+                    if 1 + len(dp[i]) > len(dp[j]):
+                        dp[j] = dp[i] + [nums[j]]
+                        if len(dp[j]) > max_len:
+                            max_len = len(dp[j])
+                            res = dp[j]
+        return res
+
 
 if __name__ == '__main__':
     l = LargestDivisibleSubset()
