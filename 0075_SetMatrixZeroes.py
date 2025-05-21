9725a24ef37b058e7df99563033c4c332bd331ff
nishant <nishant.yaji@gmail.com>
2025-05-21 15:09:48 +0530

    Solution to the leetcode problem 75

---
+++ 0075_SetMatrixZeroes.py
@@ -0,0 +1,21 @@
+# Problem 75
+from typing import List
+
+
+class SetMatrixZeroes:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row_zeros, col_zeros = set(), set()
+        rows, cols = len(matrix), len(matrix[0])
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    row_zeros.add(r)
+                    col_zeros.add(c)
+
+        for r in range(rows):
+            for c in range(cols):
+                if r in row_zeros or c in col_zeros:
+                    matrix[r][c] = 0
