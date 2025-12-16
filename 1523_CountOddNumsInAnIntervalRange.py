12bb6759cee06af5ecf831ec37b30f5cf007645f
nishant <nishant.yaji@gmail.com>
2025-12-16 11:11:34 +0530

    Solution to the leetcode problem 1523

---
+++ 1523_CountOddNumsInAnIntervalRange.py
@@ -0,0 +1,7 @@
+# Problem 1523
+
+class CountOddNumsInAnIntervalRange:
+    def countOdds(self, low: int, high: int) -> int:
+        low = low if low % 2 == 0 else low - 1
+        high = high if high % 2 == 0 else high + 1
+        return (high - low) // 2
